1fe1646fc6c1100ea64722fb427bb5607c2e0f32
MrStealYourRims <86498461+MrStealYourRims@users.noreply.github.com>
2024-08-29 15:02:59 -0400

    Update cf-userlogin.php to php 8.3

--- game/cf-userlogin.php
+++ game/cf-userlogin.php
@@ -1,102 +1,102 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * AlphaFable (DragonFable Private Server)
  * Made by MentalBlank
- * File: cf-userlogin.php - v0.1.1
+ * File: cf-userlogin.php - v0.1.2 (Updated to PHP 8.3)
  */
 
-require("../includes/classes/Core.class.php");
-require("../includes/classes/Security.class.php");
-require("../includes/classes/Ninja.class.php");
-require('../includes/config.php');
+require_once "../includes/classes/Core.class.php";
+require_once "../includes/classes/Security.class.php";
+require_once "../includes/classes/Ninja.class.php";
+require_once '../includes/config.php';
 
 $Core->makeXML();
 
 try {
-    $HTTP_RAW_POST_DATA = file_get_contents('php://input');
-    if (isset($HTTP_RAW_POST_DATA) && !empty($HTTP_RAW_POST_DATA)) {
-        $NinjaXml = $HTTP_RAW_POST_DATA;
-        $xml = new SimpleXMLElement($NinjaXml);
+    $rawPostData = file_get_contents('php://input');
+    if (!empty($rawPostData)) {
+        $xml = new SimpleXMLElement($rawPostData);
 
-        if (isset($xml->strUsername) && isset($xml->strPassword)) {
-            $username = $xml->strUsername;
-            $password = $Security->encode($xml->strPassword);
-            $query = [];
-            $result = [];
+        if (isset($xml->strUsername, $xml->strPassword)) {
+            $username = (string)$xml->strUsername;
+            $password = $Security->encode((string)$xml->strPassword);
 
-            $query[0] = $MySQLi->query("SELECT * FROM df_users WHERE name = '{$username}' AND pass = '{$password}' LIMIT 1");
-            $result[0] = $query[0]->fetch_array();
+            $userQuery = $MySQLi->query("SELECT * FROM df_users WHERE name = ? AND pass = ? LIMIT 1");
+            $userQuery->bind_param("ss", $username, $password);
+            $userQuery->execute();
+            $userResult = $userQuery->get_result()->fetch_assoc();
 
-            if ($query[0]->num_rows != 0) {
-                $query[1] = $MySQLi->query("SELECT * FROM df_settings LIMIT 1");
-                $result[1] = $query[1]->fetch_assoc();
+            if ($userResult) {
+                $settingsQuery = $MySQLi->query("SELECT * FROM df_settings LIMIT 1");
+                $settingsResult = $settingsQuery->fetch_assoc();
 
-                $CanPlay = $Security->checkAccessLevel($result[0]['access'], $result[1]['minAccess']);
-                switch ($CanPlay) {
-                    case ("Banned"):
-                        $Core->returnXMLError('Banned!', 'You have been <b>banned</b> from <b>AlphaFable</b>. If you believe this is a mistake, please contact the <b>AlphaFable</b> Staff.');
-                        break;
-                    case ("Invalid"):
-                        $Core->returnXMLError('Invalid Rank!', 'Sorry, The server is currently unavailable for your account, this may be due to server testing or upgrades. If you believe this is a mistake, please contact the <b>AlphaFable</b> Staff.');
-                        break;
-                    case ("OK"):
-                    default:
-                        break;
-                }
+                $canPlay = $Security->checkAccessLevel($userResult['access'], $settingsResult['minAccess']);
+                match ($canPlay) {
+                    "Banned" => $Core->returnXMLError('Banned!', 'You have been <b>banned</b> from <b>AlphaFable</b>. If you believe this is a mistake, please contact the <b>AlphaFable</b> Staff.'),
+                    "Invalid" => $Core->returnXMLError('Invalid Rank!', 'Sorry, The server is currently unavailable for your account, this may be due to server testing or upgrades. If you believe this is a mistake, please contact the <b>AlphaFable</b> Staff.'),
+                    default => null,
+                };
 
-                $news = $result[1]['news'];
+                $news = $settingsResult['news'];
 
-                $dob = explode('T', $result[0]['dob']);
+                $dob = explode('T', $userResult['dob']);
                 $dobnew = explode('-', $dob[0]);
-                if ($dobnew[0] . "-" . $dobnew[1] == date('m') . "-" . date('j')) {
-                    $news = $news . "<br /><br /><b>Happy Birthday!</b>";
+                if ($dobnew[0] . "-" . $dobnew[1] === date('m-j')) {
+                    $news .= "<br /><br /><b>Happy Birthday!</b>";
                 }
 
-                $MySQLi->query("UPDATE `df_users` SET `lastLogin` = '{$dateToday}' WHERE `df_users`.`id` = {$result[0]['id']};");
+                $MySQLi->query("UPDATE `df_users` SET `lastLogin` = ? WHERE `id` = ?");
+                $MySQLi->bind_param("si", $dateToday, $userResult['id']);
+                $MySQLi->execute();
 
-                $token = strtoupper(md5($Security->encode($Ninja->encryptNinja(md5(md5(strlen($token)) . md5($token . rand(1, 100000)))))));
-                $MySQLi->query("UPDATE `df_users` SET `LoginToken` = '{$token}' WHERE `df_users`.`id` = {$result[0]['id']};");
+                $token = strtoupper(md5($Security->encode($Ninja->encryptNinja(md5(md5(strlen((string)$token)) . md5($token . random_int(1, 100000)))))));
+                $MySQLi->query("UPDATE `df_users` SET `LoginToken` = ? WHERE `id` = ?");
+                $MySQLi->bind_param("si", $token, $userResult['id']);
+                $MySQLi->execute();
 
                 if ($MySQLi->affected_rows > 0) {
                     $dom = new DOMDocument();
                     $XML = $dom->appendChild($dom->createElement('characters'));
 
                     $user = $XML->appendChild($dom->createElement('user'));
-                    $user->setAttribute('UserID', $result[0]['id']);
-                    $user->setAttribute('intCharsAllowed', $result[0]['chars_allowed']);
-                    $user->setAttribute('intAccessLevel', $result[0]['access']);
-                    $user->setAttribute('intUpgrade', $result[0]['upgrade']);
-                    $user->setAttribute('intActivationFlag', $result[0]['activation']);
-                    $user->setAttribute('strUsername', $result[0]['name']);
+                    $user->setAttribute('UserID', (string)$userResult['id']);
+                    $user->setAttribute('intCharsAllowed', (string)$userResult['chars_allowed']);
+                    $user->setAttribute('intAccessLevel', (string)$userResult['access']);
+                    $user->setAttribute('intUpgrade', (string)$userResult['upgrade']);
+                    $user->setAttribute('intActivationFlag', (string)$userResult['activation']);
+                    $user->setAttribute('strUsername', $userResult['name']);
                     $user->setAttribute('strPassword', $password);
                     $user->setAttribute('strToken', $token);
-                    $user->setAttribute('strNews', "{$news}");
+                    $user->setAttribute('strNews', $news);
                     $user->setAttribute('strServerBuild', 'v0.0.1');
-                    $user->setAttribute('bitAdFlag', $result[0]['ad_flag']);
+                    $user->setAttribute('bitAdFlag', (string)$userResult['ad_flag']);
                     $user->setAttribute('strServer', 'Private Server');
                     $user->setAttribute('dateToday', $dateToday);
-                    $user->setAttribute('strDOB', $result[0]['dob']);
+                    $user->setAttribute('strDOB', $userResult['dob']);
 
-                    $i = 0;
-                    $query[2] = $MySQLi->query("SELECT * FROM df_characters WHERE userid = '{$result[0]['id']}'");
-                    while ($result[2] = $query[2]->fetch_array() and $i <= $query[2]->num_rows) {
+                    $charactersQuery = $MySQLi->query("SELECT * FROM df_characters WHERE userid = ?");
+                    $charactersQuery->bind_param("i", $userResult['id']);
+                    $charactersQuery->execute();
+                    $charactersResult = $charactersQuery->get_result();
+
+                    while ($characterData = $charactersResult->fetch_assoc()) {
                         $characters = $user->appendChild($dom->createElement('characters'));
-                        $characters->setAttribute('CharID', $result[2]['id']);
-                        $characters->setAttribute('strCharacterName', $result[2]['name']);
-                        $characters->setAttribute('intLevel', $result[2]['level']);
-                        $characters->setAttribute('intAccessLevel', $result[2]['access']);
-                        if ($result[0]['upgrade'] == 1 || $result[2]['dragon_amulet'] == 1) {
-                            $characters->setAttribute('intDragonAmulet', 1);
-                        } else {
-                            $characters->setAttribute('intDragonAmulet', 0);
-                        }
+                        $characters->setAttribute('CharID', (string)$characterData['id']);
+                        $characters->setAttribute('strCharacterName', $characterData['name']);
+                        $characters->setAttribute('intLevel', (string)$characterData['level']);
+                        $characters->setAttribute('intAccessLevel', (string)$characterData['access']);
+                        $characters->setAttribute('intDragonAmulet', ($userResult['upgrade'] == 1 || $characterData['dragon_amulet'] == 1) ? '1' : '0');
                         $characters->setAttribute('strRaceName', "Human");
-                        $characters->setAttribute('orgClassID', $result[2]['classid']);
-                        $query[3] = $MySQLi->query("SELECT * FROM df_class WHERE ClassID = '{$result[2]['classid']}'");
-                        $result[3] = $query[3]->fetch_assoc();
-                        $characters->setAttribute('strClassName', $result[3]['ClassName']);
-                        $i++;
+                        $characters->setAttribute('orgClassID', (string)$characterData['classid']);
+
+                        $classQuery = $MySQLi->query("SELECT ClassName FROM df_class WHERE ClassID = ?");
+                        $classQuery->bind_param("i", $characterData['classid']);
+                        $classQuery->execute();
+                        $classResult = $classQuery->get_result()->fetch_assoc();
+                        $characters->setAttribute('strClassName', $classResult['ClassName']);
                     }
                 } else {
                     $Core->returnXMLError('Error!', 'There was an issue updating your account information.');
@@ -105,16 +105,19 @@ try {
                 $Core->returnXMLError('User Not Found', 'Your username or / and password were incorrect, Please check your spelling and try again.');
             }
         } else {
-            $MySQLi->query("INSERT INTO `df_error_logs` (`File`, `ErrorReason`, `PlayerID`, `UserID`) VALUES ('cf-userlogin', 'Database Communication Error', '0', '0');");
+            $MySQLi->query("INSERT INTO `df_error_logs` (`File`, `ErrorReason`, `PlayerID`, `UserID`) VALUES ('cf-userlogin', 'Database Communication Error', '0', '0')");
             $Core->returnXMLError('Error!', 'There was an error communicating with the database.');
         }
     } else {
         $Core->returnXMLError('Invalid Data!', 'Message');
     }
 } catch (Exception $e) {
-    $error = mysqli_real_escape_string($MySQLi, $e->getMessage());
-    $MySQLi->query("INSERT INTO `df_error_logs` (`File`, `ErrorReason`, `PlayerID`, `UserID`) VALUES ('cf-userlogin', '{$error}', '0');");
+    $error = $MySQLi->real_escape_string($e->getMessage());
+    $MySQLi->query("INSERT INTO `df_error_logs` (`File`, `ErrorReason`, `PlayerID`, `UserID`) VALUES ('cf-userlogin', ?, '0', '0')");
+    $MySQLi->bind_param("s", $error);
+    $MySQLi->execute();
     $Core->returnXMLError('Error!', 'There was an error communicating with the database.');
 }
+
 echo $dom->saveXML();
 $MySQLi->close();
