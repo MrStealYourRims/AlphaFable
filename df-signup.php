95c7a67fa29c4d13681d7445e4a97f2b66395547
MrStealYourRims <86498461+MrStealYourRims@users.noreply.github.com>
2024-08-29 15:05:44 -0400

    Update df-signup.php to php 8.3

--- df-signup.php
+++ df-signup.php
@@ -1,45 +1,63 @@
 <?php
-require("includes/config.php");
-$query = $MySQLi->query("SELECT * FROM df_settings LIMIT 1");
-$fetch = $query->fetch_assoc();
-$signupSWF = $fetch['signupSWF'];
-$sitename = $fetch['DFSitename'];
-$MySQLi->close();
+declare(strict_types=1);
+
+require_once "includes/config.php";
+
+$settings = fetchSettings($MySQLi);
+$signupSWF = $settings['signupSWF'];
+$sitename = $settings['DFSitename'];
+
+function fetchSettings(mysqli $MySQLi): array {
+    $query = $MySQLi->query("SELECT * FROM df_settings LIMIT 1");
+    $settings = $query->fetch_assoc();
+    $MySQLi->close();
+    return $settings;
+}
+
 ?>
+<!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-    <title><?php echo $sitename; ?> | Register</title>
-    <base href=""/>
-    <link rel="stylesheet" href="includes/css/style.css"/>
-    <link rel="shortcut icon" href="includes/favicon.ico"/>
-
-    <meta charset="utf-8"/>
+    <meta charset="utf-8">
+    <title><?= htmlspecialchars($sitename) ?> | Register</title>
+    <base href="">
+    <link rel="stylesheet" href="includes/css/style.css">
+    <link rel="shortcut icon" href="includes/favicon.ico">
     <!--[if lt IE 9]>
-    <script src="https://raw.githubusercontent.com/aFarkas/html5shiv/master/src/html5shiv.js"></script><![endif]-->
+    <script src="https://raw.githubusercontent.com/aFarkas/html5shiv/master/src/html5shiv.js"></script>
+    <![endif]-->
 </head>
 <body>
-<section id="window">
-    <section id="outsideWindow">
-        <section id="gameWindow">
-            <embed src="<?php
-            echo($signupSWF);
-            ?>" bgcolor="#3B0100" wmode="transparent" style="border-radius:5px" scale="noborder" quality="autohigh"
-                   width="700" height="550" type="application/x-shockwave-flash"
-                   pluginspage="http://www.macromedia.com/go/getflashplayer" swLiveConnect="true"/>
-            <section id="linkWindow">
-                        <span>
-                            <a href="game/index.php">Play</a> | 
-                            <a href="df-signup.php">Register</a> | 
-                            <a href="mb-charTransfer.php">Transfer</a> | 
-                            <a href="top100.php">Top100</a> | 
-                            <a href="mb-bugTrack.php">Submit Bug</a> | 
-                            <a href="df-upgrade.php">Upgrade</a> | 
-                            <a href="account/">Account</a> |
-                            <a href="df-lostpassword.php">Lost Password</a>
-                        </span>
+    <section id="window">
+        <section id="outsideWindow">
+            <section id="gameWindow">
+                <embed 
+                    src="<?= htmlspecialchars($signupSWF) ?>"
+                    bgcolor="#3B0100"
+                    wmode="transparent"
+                    style="border-radius:5px"
+                    scale="noborder"
+                    quality="autohigh"
+                    width="700"
+                    height="550"
+                    type="application/x-shockwave-flash"
+                    pluginspage="http://www.macromedia.com/go/getflashplayer"
+                    swLiveConnect="true"
+                >
+                <section id="linkWindow">
+                    <span>
+                        <a href="game/index.php">Play</a> | 
+                        <a href="df-signup.php">Register</a> | 
+                        <a href="mb-charTransfer.php">Transfer</a> | 
+                        <a href="top100.php">Top100</a> | 
+                        <a href="mb-bugTrack.php">Submit Bug</a> | 
+                        <a href="df-upgrade.php">Upgrade</a> | 
+                        <a href="account/">Account</a> |
+                        <a href="df-lostpassword.php">Lost Password</a>
+                    </span>
+                </section>
             </section>
         </section>
     </section>
-</section>
 </body>
 </html>
